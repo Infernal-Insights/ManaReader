748a1b9a2c942e860572ad242dcef0ce33bc9c97
Derek <5898260+Infernal-Insights@users.noreply.github.com>
2025-08-07 17:14:02 -0400

    chore: fix lints and update pdf importer

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
--- test/importer_test.dart
+++ test/importer_test.dart
@@ -11,97 +11,6 @@ import 'package:mana_reader/importers/folder_importer.dart';
 import 'package:mana_reader/importers/zip_importer.dart';
 import 'package:mana_reader/importers/seven_zip_importer.dart';
 import 'package:mana_reader/importers/pdf_importer.dart';
-import 'package:native_pdf_renderer/native_pdf_renderer.dart';
-import 'package:native_pdf_renderer/src/renderer/interfaces/platform.dart';
-import 'dart:typed_data';
-import 'dart:async';
-
-class _FakePdfxPlatform extends PdfxPlatform {
-  @override
-  Future<PdfDocument> openFile(String filePath, {String? password}) async =>
-      _FakePdfDocument();
-
-  @override
-  Future<PdfDocument> openAsset(String name, {String? password}) async =>
-      _FakePdfDocument();
-
-  @override
-  Future<PdfDocument> openData(FutureOr<Uint8List> data,
-          {String? password}) async =>
-      _FakePdfDocument();
-}
-
-class _FakePdfDocument extends PdfDocument {
-  _FakePdfDocument()
-      : super(sourceName: 'fake.pdf', id: '1', pagesCount: 1);
-
-  @override
-  Future<void> close() async {}
-
-  @override
-  Future<PdfPage> getPage(int pageNumber, {bool autoCloseAndroid = false}) async =>
-      _FakePdfPage(this, pageNumber);
-
-  @override
-  bool operator ==(Object other) => identical(this, other);
-
-  @override
-  int get hashCode => super.hashCode;
-}
-
-class _FakePdfPage extends PdfPage {
-  _FakePdfPage(PdfDocument doc, int num)
-      : super(
-          document: doc,
-          id: 'page$num',
-          pageNumber: num,
-          width: 1,
-          height: 1,
-          autoCloseAndroid: false,
-        );
-
-  @override
-  Future<PdfPageImage?> render({
-    required double width,
-    required double height,
-    PdfPageImageFormat format = PdfPageImageFormat.jpeg,
-    String? backgroundColor,
-    Rect? cropRect,
-    int quality = 100,
-    bool forPrint = false,
-    bool removeTempFile = true,
-  }) async {
-    final bytes = base64Decode(
-        'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAiMB7g6lbYkAAAAASUVORK5CYII=');
-    return _FakePdfPageImage(pageNumber, bytes, format, quality);
-  }
-
-  @override
-  Future<PdfPageTexture> createTexture() => throw UnimplementedError();
-
-  @override
-  Future<void> close() async {}
-}
-
-class _FakePdfPageImage extends PdfPageImage {
-  _FakePdfPageImage(
-      int page, Uint8List bytes, PdfPageImageFormat format, int quality)
-      : super(
-          id: 'img$page',
-          pageNumber: page,
-          width: 1,
-          height: 1,
-          bytes: bytes,
-          format: format,
-          quality: quality,
-        );
-
-  @override
-  bool operator ==(Object other) => identical(this, other);
-
-  @override
-  int get hashCode => super.hashCode;
-}
 
 class _FakePathProviderPlatform extends PathProviderPlatform {
   final Directory tempDir = Directory.systemTemp.createTempSync(
@@ -240,7 +149,6 @@ zipfile.ZipFile(archive).extractall(dest)
     });
 
     test('PdfImporter renders pages', () async {
-      PdfxPlatform.instance = _FakePdfxPlatform();
       final tmp = Directory.systemTemp.createTempSync();
       final pdfData = base64Decode(
         'JVBERi0xLjEKMSAwIG9iajw8L1R5cGUvQ2F0YWxvZy9QYWdlcyAyIDAgUj4+ZW5kb2JqCjIgMCBvYmo8PC9UeXBlL1BhZ2VzL0tpZHNbMyAwIFJdL0NvdW50IDE+PmVuZG9iagozIDAgb2JqPDwvVHlwZS9QYWdlL1BhcmVudCAyIDAgUi9NZWRpYUJveFswIDAgNjEyIDc5Ml0+PmVuZG9iagp0cmFpbGVyPDwvUm9vdCAxIDAgUi9TaXplIDQ+PgolJUVPRg==',
